df36d2cc5832f9713b4a54938e645d258f2e088e
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-11 17:52:55 +0900

    #2. PayPlan 스키마 생성 및 superuser 생성
    models.py에 PayPlan 스키마 생성 및 설정을 합니다.
      - name
      - price
      - updated_at
      - created_at
    참고로 id는 Django에서 BigIntegerField로 BigAutoField(Auto Increment)하게 생성해줍니다. 
    
    settings.py의 INSTALLED_APPS 리스트에 우리가 만든 웹앱을 element로 추가시킵니다. 그럼 Django가 이를 토대로 우리의 웹앱을 관리합니다.
      - "shortener"
    
    스키마에 정의한대로 테이블을 생성합니다.
      - python manage.py makemigrations
      - 우리 웹앱(shortener)에 migrations 디렉터리에 0001_initial.py 라는 파일이 생깁니다.
    
    실제 DB(sqlite3)에 이주(등록)시킵니다. 
      - python manage.py migrate
    
    superuser를 생성합니다.
      - python manage.py createsuperuser
      - 그럼 username, password, email 등을 물어보고 원하는 것을 입력하시면 됩니다. 
      - 데이터베이스에 초기로 User테이블(auth_user)이 있는데 여기에 admin이라는 이름의 user를 추가시키는 것입니다. 
    
    데이터베이스는 추후에 PostgreSQL이나 MySQL로 관리할 것이지만, 관리 도구는 아직 선택하지 않았습니다. 우선은 DBeaver를 통해 관리할 것이기 때문에 DBeaver를 설치합니다.

--- shrinkers/shortener/models.py
+++ shrinkers/shortener/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# class Users(AbstractUser):
+#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
